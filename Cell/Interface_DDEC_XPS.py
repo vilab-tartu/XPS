239e620b93c43e356b629bb42ed43eaa9919b10f
heigoers <heigoers@ut.ee>
2020-01-23 12:52:50 +0200

    Refactored code, added slicing, first calculated examples of Neutral and Positive systems

--- Cell/Interface_DDEC_XPS.py
+++ Cell/Interface_DDEC_XPS.py
@@ -42,11 +42,14 @@ def process_command_line(argv):
                         help="""How many times You want to replicate original Cell for a supercell in each direction for calculating correction,
                          separated by comma e.g. 3,3,1""")
 
+    parser.add_argument('-slices',
+                        help="""Point out where you want to slice the cell to obtain XPS spectra of different parts of cell,
+                         separated by comma e.g. 0,20,30,50""")
+
     # Optional args
     parser.add_argument('-c',
                         help="""The index of alkylic carbon in .xyz coordinates (The first atom in .xyz is 0, second 1....)""")
     return parser.parse_args(argv)
-
 
 """ 
     Function used for calculating binding energies of carbons' electron's for XPS spectra
@@ -60,6 +63,30 @@ def process_command_line(argv):
 def fun(Z, c):
     chg, corr = Z
     return 13.45 * np.asarray(chg) + 14.4 * np.asarray(corr) + c
+
+
+""" 
+    Wrapper function for calculating binding energies of carbons' electron's for XPS spectra
+
+    Arguments:
+        commandLineArgs - arguments given in command line
+        carbonDf - the dataframe containing carbon atoms, which XPS spectra is to be estimated
+    Returns:
+        Array of binding energies in eVs
+"""
+def calculateBEs(commandLineArgs, carbonDf):
+    if type(commandLineArgs.c) == type(None):
+        # If no alcylic carbon given do not fit anything, just calculate BE values using data and auxilliary variable 0
+        BEs = fun([list(carbonDf['charge'])] + [list(carbonDf['correction'])], 0)
+    else:
+        # Alkylic carbon's energy is 285 eV
+        alkyl_carbon = carbonDf.iloc[int(commandLineArgs.c)]  # Select alkylic carbon
+        # Find the constant value
+        popt, pcov = curve_fit(fun, [alkyl_carbon['charge'], alkyl_carbon['correction']], 285)
+        # Estimate the BE values by using the function and optimized constant
+        # print("Constant value is: " + str(popt))
+        BEs = fun([list(carbonDf['charge'])] + [list(carbonDf['correction'])], *popt)  # Using function to estimate BEs
+    return BEs
 
 """
     Function for estimating the correction electrostatic potential of the other atoms to carbon's BE
@@ -106,21 +133,79 @@ def readDDEC(DDECFilename):
     return data
 
 """
-    Method for testing giving filename instead of command line excecution
+    Function for binning the calculated BE-s to obtain intensity
+
+    Arguments:
+        BEs - array of calculated binding energies
+
+    Returns:
+        2D list, first list is created BE axis, second list is intensity
+"""
+def findBEAxisAndIntensity(BEs):
+    # Create arbitrary X-axis
+    BE_axis = np.linspace(min(BEs) - 1, max(BEs) + 1, 100)  # eV
+    # Create intenstity Y-axis
+    arb_intensity = np.zeros(len(BE_axis))
+    # Way to find indexes of points, where set intensity 1
+    indexes = np.round((np.sort(BEs) - min(BE_axis)) / (abs(max(BE_axis) - min(BE_axis)) / len(BE_axis)))
+    for i in indexes:
+        arb_intensity[int(i)] += 1
+    return [BE_axis, arb_intensity]
+
+"""
+    Function for creating figures of calculated XPS spectra and saving them
+
+    Arguments:
+        commandLineArgs - Arguments from command line (only necessary is filename of DDEC6 analysis)
+        BE - array of calculated binding energies
+
+    Returns:
+        Nothing, saves figures to hard drive
+"""
+def createFigureAndSave(addToFilename, BE_axis, arb_intensity):
+    # Plotting and saving data
+    f1, (ax) = plt.subplots(1, 1, sharey=False, sharex=True, figsize=(8.3 / 2.54, 8.3 / 2.54))
+    f1.subplots_adjust(hspace=0)
+    ax.plot(BE_axis, gaussian_filter(arb_intensity, 0.5), 'k')
+    ax.set_yticklabels([])
+    ax.tick_params(axis='both', labelsize=9)
+    ax.set_xlabel('BEs [eV]', fontsize=12)
+    ax.set_ylabel('Intensity [arb. units]', fontsize=12)
+    f1.savefig(addToFilename + "_spectra.png", format="png", dpi=300, bbox_inches='tight')
+    f1.savefig(addToFilename + "_spectra.svg", format="svg")
+    np.savetxt(addToFilename + "_spectra.csv", list(zip(BE_axis, gaussian_filter(arb_intensity, 2))), delimiter=',')
+
+"""
+    Function for saving calculated BEs
+
+    Arguments:
+        addToFilename - addition to data filename
+        BEs - array of calculated binding energies
+        carbonIndexes - Indexes that correspond to carbon atom indexes of BEs
+
+    Returns:
+        Nothing, saves data to hard drive
+"""
+def saveData(addToFilename, BEs, carbonIndexes):
+    df = pd.DataFrame(BEs, columns=["BE [eV]"], index=carbonIndexes)
+    df.index.name = "Atom IX"
+    df.to_csv(addToFilename + "_BE.csv")
+"""
+    Method for testing giving filename and other parameters here instead of command line execution
 """
 class test:
-    def __init__(self, filename, alkylc, xyzFilename, cell, superCell):
+    def __init__(self, filename, alkylc, xyzFilename, cell, superCell, slices):
         self.i = filename
         self.c = alkylc
         self.xyz = xyzFilename
         self.cell = cell
         self.superCell = superCell
+        self.slices = slices
 
 ######################### Main excecution starts here ###################################
 
-
 #args = process_command_line(sys.argv[1:])
-args = test("DDEC6_even_tempered_net_atomic_charges.xyz", None, "run_gr-IL_model-20151216-1-13501_mn.xyz", "34.08,34.433,50","3,3,1")
+args = test("DDEC6_-05.xyz", None, "Gr_-05_EMImBF4-4050.xyz", "34.08,34.433,50","3,3,1", "0,20,30,40")
 
 #Parse the cell length
 aux = args.cell.split(",")
@@ -168,56 +253,28 @@ centerCellDf = centerCellDf.sort_values(by='tag')
 # Calculate correction to energy from neighbouring atoms' charges to middle cell atoms
 centerCellDf['correction']=CalculateCorrCharge(centerCellDf,superCell)
 
+#Parse slicing
+sliced = args.slices.split(",")
+#Create spectras for slices and lastly for the total cell
+for i in range(len(sliced)):
+    if i == (len(sliced)-1):
+        leftBound = 0
+        rightBound = zlength
+    else:
+        leftBound = float(sliced[i])
+        rightBound = float(sliced[i+1])
 
-carbons = centerCellDf[centerCellDf['Atom'] == 'C']  # Select carbons from there
-carbonIndexes = np.asarray(centerCellDf['tag'])
-carbonIndexes = carbonIndexes[centerCellDf['Atom'] == 'C'] #Carbon indexes
+    #Select carbon atoms and estimate their binding energies
+    carbons = centerCellDf[centerCellDf['Atom'] == 'C']  # Select carbons from there
+    carbonIndexes = np.asarray(centerCellDf['tag'])
+    carbonIndexes = carbonIndexes[centerCellDf['Atom'] == 'C'] #Carbon indexes
+    carbonIndexes = carbonIndexes[(carbons['z'] >= leftBound) & (carbons['z'] <= rightBound)]
+    carbons = carbons[(carbons['z'] >= leftBound) & (carbons['z'] <= rightBound)]
+    BEs = calculateBEs(args, carbons)
+    addToFilename = args.xyz.split(".")[0]+"_"+str(leftBound)+"_"+str(rightBound)
+    BE_axis, arb_intensity = findBEAxisAndIntensity(BEs)
+    createFigureAndSave(addToFilename, BE_axis, arb_intensity)
+    saveData(addToFilename, BEs, carbonIndexes)
 
-if type(args.c) == type(None):
-    #If no alcylic carbon given do not fit anything, just calculate BE values using data and auxilliary variable 0
-    BEs = fun([list(carbons['charge'])] + [list(carbons['correction'])], 0)
-
-else:
-    # Alkylic carbon's energy is 285 eV
-    alkyl_carbon = carbons.iloc[int(args.c)]  # Select alkylic carbon
-    # Find the constant value
-    popt, pcov = curve_fit(fun, [alkyl_carbon['charge'], alkyl_carbon['correction']], 285)
-    # Estimate the BE values by using the function and optimized constant
-    # print("Constant value is: " + str(popt))
-    BEs = fun([list(carbons['charge'])] + [list(carbons['correction'])], *popt)  # Using function to estimate BEs
-
-#Adding binding energies to dataframe and
-carbons['BE'] = BEs
-
-# Create arbitrary X-axis
-BE_axis = np.linspace(min(BEs) - 1, max(BEs) + 1, 100)  # eV
-
-# Create intenstity Y-axis
-arb_intensity = np.zeros(len(BE_axis))
-
-# Way to find indexes of points, where set intensity 1
-indexes = np.round((np.sort(BEs) - min(BE_axis)) / (abs(max(BE_axis) - min(BE_axis)) / len(BE_axis)))
-for i in indexes:
-    arb_intensity[int(i)] += 1
-
-# Plotting and saving data
-f1, (ax) = plt.subplots(1, 1, sharey=False, sharex=True, figsize=(8.3 / 2.54, 8.3 / 2.54))
-f1.subplots_adjust(hspace=0)
-
-addToFilename = args.i.split(".")[0]
-
-ax.plot(BE_axis, gaussian_filter(arb_intensity, 0.5), 'k')
-ax.set_yticklabels([])
-ax.tick_params(axis='both', labelsize=9)
-
-ax.set_xlabel('BEs [eV]', fontsize=12)
-ax.set_ylabel('Intensity [arb. units]', fontsize=12)
-
-f1.savefig(addToFilename+"_spectra.png", format="png", dpi=300, bbox_inches='tight')
-f1.savefig(addToFilename+"_spectra.svg", format="svg")
-np.savetxt(addToFilename+"_spectra.csv", list(zip(BE_axis, gaussian_filter(arb_intensity, 2))), delimiter=',')
-centerCellDf.to_csv("testing.xyz", columns=["Atom",'x','y','z'], sep="\t")
+#centerCellDf.to_csv("testing.xyz", columns=["Atom",'x','y','z'], sep="\t")
 #print(df)
-df = pd.DataFrame(BEs, columns=["BE [eV]"], index=carbonIndexes)
-df.index.name = "Atom IX"
-df.to_csv(addToFilename+"_BE.csv")
